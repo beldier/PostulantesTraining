7bb11eabc925522b5a07ac1bca4753c327761738
Alvaro Yapu <alvaro.yapu.cossio@gmail.com>
2024-06-27 23:03:17 -0400

    Anadir live datas a llamada de api

--- app/src/main/java/com/rybeler/exampleviewmodel/ui/fragments/SecondFragment.kt
+++ app/src/main/java/com/rybeler/exampleviewmodel/ui/fragments/SecondFragment.kt
@@ -5,22 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.navArgs
 import com.rybeler.exampleviewmodel.data.network.ApiClient
 import com.rybeler.exampleviewmodel.databinding.FragmentSecondBinding
 import com.rybeler.exampleviewmodel.ui.adapters.CharactersAdapter
+import com.rybeler.exampleviewmodel.ui.viewModel.SecondViewModel
 import kotlinx.coroutines.launch
 
 class SecondFragment : Fragment() {
 
     private lateinit var binding: FragmentSecondBinding
-    private val args: SecondFragmentArgs  by navArgs()
-
+    private val args: SecondFragmentArgs by navArgs()
+    private val viewModel: SecondViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
@@ -30,11 +33,10 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.titleText.text = args.tituloText
 
-        lifecycleScope.launch {
-            val listCharacters = ApiClient.charactersService.getCharacters(0, 100).data.results
-            binding.recycler.adapter = CharactersAdapter(listCharacters)
-
+        viewModel.caracteres.observe(viewLifecycleOwner) { updatedList ->
+            binding.recycler.adapter = CharactersAdapter(updatedList)
         }
+
     }
 
 }
